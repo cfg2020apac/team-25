6d9382ca8e9869ad8c500a42b481e669dbb60269
bevanvarghese <bevanvar@connect.hku.hk>
2020-10-09 01:26:44 +0530

    #1 added admin SDK to firebase boilerplate

--- functions/handlers/helloWorld.js
+++ functions/handlers/helloWorld.js
@@ -1,11 +1,30 @@
-const functions = require('firebase-functions');
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-module.exports = functions.https.onRequest((request, response) => {
-  console.log("Hello logs!");
-  response.status(200).json({
-    message: "Hello World!"
-  });
-});
+
+exports.helloWorld = (req, res) => {
+  console.log('Hello logs!');
+  return res.status(200).json({ message: 'Hello World! This is team 25.' });
+};
+
+// request-type: POST
+// URL: /who
+// sample request (content-type: JSON)
+// {
+//   "message": "Who are you?"
+// }
+exports.whoAreWe = (req, res) => {
+  try {
+    console.log('Success');
+    if (req.body.message === 'Who are you?') {
+      return res
+        .status(200)
+        .json({ message: 'We are 25 $lackers. JPMorgan pls recruit us :/' });
+    } else {
+      return res.status(200).json({ message: 'Try again.' });
+    }
+  } catch (err) {
+    console.error('Failure');
+    return res.status(400).json({ error: 'Bad request ' });
+  }
+};
--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
 
-exports.helloWorld = require("./handlers/helloWorld");
+admin.initializeApp();
+// const db = admin.firestore();
+const app = express();
+app.use(cors());
+
+//handler imports
+const { helloWorld, whoAreWe } = require('./handlers/helloWorld');
+
+//routes
+app.get('/', helloWorld);
+app.post('/who/', whoAreWe);
+
+exports.api = functions.https.onRequest(app);
