a5fddab898b3ae121e749c1eb5a171482d3e626a
Victor Lam <victorlamprojects@gmail.com>
2020-10-10 01:25:10 +0800

    sidebar

--- webapp/src/Components/TopNav/index.tsx
+++ webapp/src/Components/TopNav/index.tsx
@@ -1,20 +1,83 @@
 import React, { FC, useState } from 'react';
-import Sidebar from "react-sidebar";
-import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Sidebar from 'react-sidebar';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import MenuIcon from '@material-ui/icons/Menu';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
 import getDocumentReference from '../../Utils/getDocumentReference';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     background: "#2c2c34",
   },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
   menuButton: {
     marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
 }));
 
@@ -24,8 +87,17 @@ export interface TopNavProps {
 
 const TopNav: FC<TopNavProps> = (props) => {
   const classes = useStyles();
-  const [sidebarOpen, setSideBarOpen] = useState(false);
-  let {type} = props;
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  let {type, children} = props;
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   if(type == "")
     type = "HandsOff";
   else if(type == "ngo")
@@ -36,23 +108,72 @@ const TopNav: FC<TopNavProps> = (props) => {
     type = "Volunteer";
   return (
     <div className={classes.root}>
-      <AppBar position="static" style={{backgroundColor: '#2c2c34'}}>
-        <Toolbar variant="dense">
-          <button className={classes.menuButton} style={{backgroundColor: "inherit", boxShadow: 'none'}} aria-label="menu" onClick={() => {setSideBarOpen(true)}}>
-            <MenuIcon/>
-          </button>
-          <Typography variant="h6" color="inherit">
-            {type}
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+        style={{backgroundColor: '#2c2c34'}}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            <Typography variant="h6" color="inherit">
+              {type}
+            </Typography>
           </Typography>
         </Toolbar>
       </AppBar>
-      <Sidebar
-        sidebar={<b>Sidebar content</b>}
-        open={sidebarOpen}
-        onSetOpen={()=>{setSideBarOpen(!sidebarOpen)}}
-        styles={{ sidebar: { background: "white" } }}
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
       >
-      </Sidebar>
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        {children}
+      </main>
     </div>
   );
 }
