a235d116012a49dab9053d04b1c3e6a426f3a5bf
KAY1020 <53653084+KAY1020@users.noreply.github.com>
2020-10-10 04:57:55 +0800

    Add volunteer 2

--- webapp/src/Screens/AddVolunteer/index.tsx
+++ webapp/src/Screens/AddVolunteer/index.tsx
@@ -1,13 +1,25 @@
 import React, { FC } from 'react';
-import { Box, CircularProgress } from '@material-ui/core';
+//import { Box, CircularProgress } from '@material-ui/core';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 import { useHistory } from 'react-router-dom';
-
+//import { CountryDropdown, RegionDropdown, CountryRegionData } from 'react-country-region-selector';
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
+import { makeStyles } from '@material-ui/core/styles';
 
 const db = firebase.firestore();
+
+const useStyles = makeStyles((theme) => ({
+    form: {
+        display: 'grid',
+      marginTop: theme.spacing(8),
+      flexDirection: 'column',
+      alignItems: 'center',
+    }
+  }));
+
+
 
 const volunteerSchema = yup.object().shape({
   // address: {
@@ -32,7 +44,9 @@ const volunteerSchema = yup.object().shape({
   // employmentStatus: string;
   employmentStatus: yup.string(),
   // firstName: string;
+  firstName: yup.string(),
   // gender: string;
+  gender: yup.string(),
   // lastLoggedInOn: admin.firestore.Timestamp;
   // lastModifiedOn: admin.firestore.Timestamp;
   // mainEmail: string;
@@ -60,6 +74,13 @@ const AddVolunteerScreen: FC = () => {
   const onVolunteerFormSubmit = async (formValues) => {
     // '2012-01-08'
     const currentServerTime = firebase.firestore.FieldValue.serverTimestamp();
+
+    const createdOn = firebase.firestore.FieldValue.serverTimestamp();
+
+    const lastLoggedInOn = firebase.firestore.FieldValue.serverTimestamp();
+    
+    const lastModifiedOn = firebase.firestore.FieldValue.serverTimestamp();
+    
     // Generating Firebase timestamp from JS Date object
     // var myTimestamp = firebase.firestore.Timestamp.fromDate(new Date());
 
@@ -81,10 +102,47 @@ const AddVolunteerScreen: FC = () => {
       >
         {({ isSubmitting }) => (
           <Form>
+              
+            <Field type="fullName" name="fullName" />
+            <ErrorMessage name="fullName" component="div" />
+            
+            <Field type="chineseName" name="chineseName" />
+            <ErrorMessage name="chineseName" component="div" />
+
+            <Field type="gender" name="gender" />
+            <ErrorMessage name="gender" component="div" />
+
             <Field type="email" name="email" />
             <ErrorMessage name="email" component="div" />
-            <Field type="password" name="password" />
-            <ErrorMessage name="password" component="div" />
+
+            <Field type="alternativeEmail" name="alternativeEmail" />
+            <ErrorMessage name="alternativeEmail" component="div" />
+
+            <Field type="employmentStatus" name="employmentStatus" />
+            <ErrorMessage name="employmentStatus" component="div" />
+            
+            <Field type="educationLevel" name="educationLevel" />
+            <ErrorMessage name="educationLevel" component="div" />
+
+            <Field type="volunteerSkills" name="volunteerSkills" />
+            <ErrorMessage name="volunteerSkills" component="div" />
+            
+
+            <Field type="phoneNumber" name="phoneNumber" />
+            <ErrorMessage name="phoneNumber" component="div" />
+
+            <Field type="businessNumber" name="businessNumber" />
+            <ErrorMessage name="businessNumber" component="div" />
+ 
+            <Field type="permanentIDValue" name="permanentIDValue" />
+            <ErrorMessage name="permanentIDValue" component="div" />
+
+            <Field type="permanentIDCountry" name="permanentIDCountry" />
+            <ErrorMessage name="permanentIDCountry" component="div" />
+
+            <Field type="permanentIDType" name="permanentIDType" />
+            <ErrorMessage name="permanentIDType" component="div" />
+
             <button type="submit" disabled={isSubmitting}>
               Submit
             </button>
