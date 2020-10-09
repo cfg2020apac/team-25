26ce9af1bf8484e291d315fd8e5940b338c04b27
Michael Lim <michael@eventxtra.com>
2020-10-09 23:39:26 +0800

    create submission complete component

--- webapp/src/Routes/AppNavigation.tsx
+++ webapp/src/Routes/AppNavigation.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import { LinearProgress } from '@material-ui/core';
 import Page404 from '../Screens/Page404';
 import lazyImport from '../lazy-import';
+import Complete from '../Screens/Complete';
 
 const Home = lazyImport('../Screens/Home');
 const Admin = lazyImport('../Screens/Admin');
@@ -18,6 +19,7 @@ const Appnavigation: FC = () => {
           <Route path='/admin' exact component={Admin} />
           <Route path='/ngo' exact component={NGO} />
           <Route path='/volunteer' exact component={Volunteer} />
+          <Route path='/complete' exact component={Complete} />
           <Route path='/*' exact component={Page404} />
         </Switch>
       </Suspense>
