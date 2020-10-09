27bf5aee194b067ed7827fb97fc072fa20ef987a
bevanvarghese <bevanvar@connect.hku.hk>
2020-10-10 04:58:32 +0530

    graph data added

---
+++ webapp/dummyGraph.js
@@ -0,0 +1,66 @@
+const dummyData = [
+  {
+    title: 'Volunteers by Gender',
+    values: [
+      [
+        15,
+        84
+      ],
+      [
+        "Male",
+        "Female"
+      ]
+    ],
+  },
+  {
+    title: 'Most Popular Event Types',
+    values: [
+      [
+        221,
+        74,
+        28,
+        26,
+        54
+      ],
+      [
+        "Youth Opportunities",
+        "Serve-A-Thon",
+        "COVID-19 Relief",
+        "Phone Call Support",
+        "Others",
+      ]
+    ],
+  },
+  {
+    //NOT BASED ON ACTUAL DATA 
+    title: 'Volunteers by Language',
+    values: [
+      [
+        40,
+        32,
+        27,
+      ],
+      [
+        "English",
+        "Cantonese",
+        "Both",
+      ]
+    ],
+  },
+  {
+    //NOT BASED ON ACTUAL DATA 
+    title: 'Volunteers by Residential Status',
+    values: [
+      [
+        68,
+        31,
+      ],
+      [
+        "Locals",
+        "Expats",
+      ]
+    ],
+  },
+
+];
+export default dummyData;
