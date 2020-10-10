dc9e1d24c6b24154e065431ae9a6eeca7f74c9d7
Michael Lim <michael@eventxtra.com>
2020-10-10 08:13:40 +0800

    add volunteer data to admin panel

--- webapp/src/Screens/Admin/index.tsx
+++ webapp/src/Screens/Admin/index.tsx
@@ -1,25 +1,74 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import { Box, CircularProgress, Typography } from '@material-ui/core';
-import { useDocumentData } from 'react-firebase-hooks/firestore';
-import getDocumentReference from '../../Utils/getDocumentReference';
+import getCollectionReference from '../../Utils/getCollectionReference';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import MUIDataTable from "mui-datatables";
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const db = firebase.firestore();
 
 export interface AdminScreenProps {}
 
+const columns = [
+  {
+   name: "firstName",
+   label: "First Name",
+   options: {
+    filter: false,
+    sort: true,
+   }
+  },
+  {
+    name: "lastName",
+    label: "Last Name",
+    options: {
+     filter: false,
+     sort: true,
+    }
+   }, 
+  {
+    name: "mainEmail",
+    label: "Email",
+    options: {
+     filter: false,
+     sort: true,
+    }
+  },
+];
+
+// const deleteUser = ({ id }) => db.collection('volunteers').doc(id).delete();
+
 const AdminScreen: FC<AdminScreenProps> = (props) => {
-  const [indexData, loading] = useDocumentData(getDocumentReference('display', 'index'));
+  const [values, loading, error] = useCollectionData(getCollectionReference('volunteers'), {
+    idField: 'volunteerId'
+  });
+
+  const options = useMemo(() => ({
+    filterType: 'checkbox',
+    download: false,
+    onRowsDelete: async (lookup, data) => {
+      const idToDelete = (values[lookup.data[0].index] as any).volunteerId;
+      await db.collection('volunteers').doc(idToDelete).delete();
+    }
+  }), [values]);  
 
   if (loading) {
     return <CircularProgress size="large" />
   }
 
-  const { heading, message } = (indexData || {}) as {
-    heading: string;
-    message: string;
-  };
+  if (error) {
+    return (<div>Error fetching volunteers</div>);
+  }
 
   return (
     <Box>
-      <Typography component="h1" variant="h1">Admin</Typography>
+      <MUIDataTable
+        title={"Volunteers List"}
+        data={values}
+        columns={columns}
+        options={options}
+      />
     </Box>
   )
 }
