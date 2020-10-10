04315682078485415fe434ff5bf5171cdfc105d6
XDMickeyYau <yaurf44@gmail.com>
2020-10-10 08:15:35 +0800

    some special

--- webapp/src/Boot.ts
+++ webapp/src/Boot.ts
@@ -1,22 +1,21 @@
-import Config from './config';
-import axios from 'axios';
+import Config from "./Config";
+import axios from "axios";
 
-const VALIDATE_CONFIG_PROPERTIES = ['BASE_URL'];
+const VALIDATE_CONFIG_PROPERTIES = ["BASE_URL"];
 
 const validateConfig = () => {
-  VALIDATE_CONFIG_PROPERTIES.forEach(key => {
+  VALIDATE_CONFIG_PROPERTIES.forEach((key) => {
     const val = Config.get(key);
-    if (!val)
-      throw new Error(`App config must define ${key}`);
+    if (!val) throw new Error(`App config must define ${key}`);
   });
-}
+};
 
 const Boot = () => {
   return new Promise((resolve, reject) => {
     validateConfig();
-    axios.defaults.baseURL = Config.get('BASE_URL');
+    axios.defaults.baseURL = Config.get("BASE_URL");
     resolve();
-  })
+  });
 };
 
 export default Boot;
