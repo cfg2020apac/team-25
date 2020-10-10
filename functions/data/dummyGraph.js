164de2232dd29ba3959a44e5c5167e71eacd51de
Michael Lim <michael@eventxtra.com>
2020-10-10 08:18:27 +0800

    add event participation rate api

--- functions/data/dummyGraph.js
+++ functions/data/dummyGraph.js
@@ -82,4 +82,5 @@ const dummyData = [
   },
 
 ];
-export default dummyData;
+
+module.exports = dummyData;
--- functions/handlers/volunteersByLanguage.js
+++ functions/handlers/volunteersByLanguage.js
@@ -1,7 +1,7 @@
 const dummyData = require("../data/dummyGraph");
 
 const volunteersByLanguage = (req, res) => {
-  res.status(200).json(dummyData[2]);
+  res.status(200).json(dummyData[3]);
 }
 
 module.exports = volunteersByLanguage;
